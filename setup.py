415eb6865a003f1dd03c16f5f816c9c3268ce02f
James Brookes <james.brookes@stfc.ac.uk>
2022-07-19 20:56:56 +0100

    Switch to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,21 +1,3 @@
-# pylint:skip-file
-"""
-Wrapper for the functionality for various installation and project setup commands
-see:
-    `python setup.py help`
-for more details
-"""
-from setuptools import setup, find_packages
+from setuptools import setup
 
-# This file is made so that the CI can build a package instead of failing the step.
-#  There's no intent on ever publishing this package.
-
-setup(name="autoreduce_actions",
-      version="1.0.0",
-      description="ISIS Autoreduce common Github actions",
-      author="ISIS Autoreduction Team",
-      url="https://github.com/autoreduction/autoreduce-actions/",
-      packages=find_packages(),
-      classifiers=[
-          "Programming Language :: Python :: 3 :: Only",
-      ])
+setup()
